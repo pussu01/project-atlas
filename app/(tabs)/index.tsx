4d7a6fff22da32b502f09369d7c6353129f61a7e
Pushpendra <pussu01@gmail.com>
2026-08-17 17:37:51 +0530

    Initial Atlas project setup

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -18,7 +18,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
+        <ThemedText type="title">Hello Atlas - Day 1</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
